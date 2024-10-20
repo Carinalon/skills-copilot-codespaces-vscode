4563889f2aad871e321094f0b41aa6daa239d2e7
Carina <146588583+Carinalon@users.noreply.github.com>
2024-10-20 16:26:03 +0000

    correccion

--- comments.js
+++ comments.js
@@ -1,1 +1,21 @@
 // Create wev server
+// import module
+const express = require('express');
+const app = express.Router();
+const commentController = require('./controllers/commentController');
+
+// handle request
+router.get('/', commentController.comments_list);
+router.get('/create', commentController.comments_create_get);
+router.post('/create', commentController.comments_create_post);
+router.get('/:id/delete', commentController.comments_delete_get);
+router.post('/:id/delete', commentController.comments_delete_post);
+
+
+//app.get('/', (req, res) => {
+    res.send('Hello World');
+    //});
+    
+//app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+//});
